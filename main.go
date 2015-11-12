5f3c67aaddf8422502fab98907e6db36a0e7a4c6
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2015-11-13 00:12:24 +0400

    Add configs for DB, with some info in it

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	_ "github.com/nodar-chkuaselidze/bee-api-test/docs"
+	_ "github.com/nodar-chkuaselidze/bee-api-test/models"
 	_ "github.com/nodar-chkuaselidze/bee-api-test/routers"
 
 	"github.com/astaxie/beego"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
 )
 
+func init() {
+	orm.RegisterDriver("postgres", orm.DR_Postgres)
+	orm.RegisterDataBase("default", "postgres", "user=nod dbname=BeeAPI sslmode=disable")
+}
+
 func main() {
+	orm.RunCommand()
+
 	if beego.RunMode == "dev" {
 		beego.DirectoryIndex = true
 		beego.StaticDir["/swagger"] = "swagger"
